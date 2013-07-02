e91c491a9ff6a5704302d7427d49463aa4681be7
Javier Maldonado <jmaldonado6253@gmail.com>
2013-07-02 23:25:39 +0000

    list completion, js broken

--- js/speakers.js
+++ js/speakers.js
@@ -1,14 +1,14 @@
-	$(document).ready(function () {
-	    $('input').typeahead({
-	        name: 'countries',
-	        local: ['United States of America', 'Democratic Peoples Republic of Korea', 'Mali']
-	    });
-	});
+document.getElementById("namebox").addEventListener("keydown", function(e) {
+    if (!e) { var e = window.event; }
+        // Enter is pressed
+        if (e.keyCode == 13) { sendName(); }
+    }, false);
+
 	var options = {
 	    item: '<li><hr class="separator" /><h4 class="name"></h4></li>'
 	};
 
-	var values = [{name:'Woowoowoo'}];
+	var values = [{name:'Clear this'}];
 
 	var speakersList = new List('speakers-list', options, values);
 
@@ -18,3 +18,21 @@
 	    speakersList.add({name: sent});
 		textbox.value='';
 	}
+
+function startTimer() {
+    var timer = document.getElementById("time");
+    while(timer.textContent != 0:00:00)
+    {
+        setTimeout(1000);
+        var arr = timer.textContent.split(":");
+        window.alert(timer.textContent);
+        timer.textContent = "0:00:00";
+    }
+}
+//JQuery Stuff
+$(document).ready(function () {
+	    $('input').typeahead({
+	        name: 'countries',
+	        local: ['United States of America', 'Democratic Peoples Republic of Korea', 'Mali']
+	    });
+	});
