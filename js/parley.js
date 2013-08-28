16540ee276ceaa3f4ba3a0e8aab7bb8c4aa19a7a
Javier Maldonado <jmaldonado6253@gmail.com>
2013-08-28 14:18:59 +0000

    renamed files, built more timer

--- js/parley.js
+++ js/parley.js
@@ -12,7 +12,7 @@ var currentID = 1;
 var defaultTime=30;
 var timerPosition=0; //number of seconds the timer should display
 var timeLeft=0; //number of seconds the timer should run, resets to this value after timeout
-var timereg=/\d\d:\d\d:\d\d/
+var timereg=^(([0-5])?\d:)?\d\d\s*$ //i had a problem, so I used a regex. now it's the next guy's problem
 
 function init() {
 	$('input').typeahead({name: 'countries', local: ['United States of America', 'Democratic Peoples Republic of Korea', 'Mali']});
@@ -44,26 +44,44 @@ function nextId() {
     return currentID;
 }
 
+var start = new Date().getTime(), elapsed = '0.0' ticking = false;
+
+window.setInterval(function()
+        {
+            var time = new Date().getTime() - start;
+            elapsed = Math.floor(time / 100) / 10;
+            if(Math.round(elapsed) == elapsed) { elapsed += '.0'; }
+        }, 100);
+
 //Has to be able to accept "30" as 30 seconds and "1:30" as 90 seconds
 function setTimer() {
     stopTimer();
     var input = $("#timebox").val();
-    //TODO add real time validation
-    if(input > 60)
+    if(timereg.test(input))
         timeLeft = input;
     else
-        alert("stahp");
-
+        alert("stahp"); //add a visual bell
 }
 
 function toggleTimer() {
-    
+   if(ticking)
+       stopTimer();
+   else
+       startTimer();
 }
 
 function startTimer() {
+    ticking = true;
+    var begin = elapsed;
+    console.log(begin);
+    while(begin != (begin+timeLeft))
+    {
+       $( 
+    }
 }
 
 function stopTimer() {
+    ticking = false;
 }
 
 //Event Listener
